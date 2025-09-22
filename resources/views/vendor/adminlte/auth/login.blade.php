ab1b8140cfd8cdbbb7557b6e60e78d62d60526a4
juan.diaz <rulos26@gmail.com>
2025-09-21 21:09:41 -0500

    Actualiza las rutas de las imágenes de logo en la configuración de AdminLTE y modifica los enlaces de los CDN en las vistas para eliminar las integridades. Se mejora la carga de recursos al utilizar enlaces directos.

--- resources/views/vendor/adminlte/auth/login.blade.php
+++ resources/views/vendor/adminlte/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('adminlte_css_pre')
     <!-- iCheck Bootstrap CDN -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/icheck-bootstrap/3.0.1/icheck-bootstrap.min.css" integrity="sha512-8vq2g5nHE062j3xor4XxPeZiPjmRDh6wlufQlfC6pdQ/9urJkU07NM0tEREeybP8VYS20BCVv8vUDl8R7Wg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/icheck-bootstrap/3.0.1/icheck-bootstrap.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
 @stop
 
 @php
